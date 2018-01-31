3a001a160e3d2023810ae1f4b7d9ae8abb30af75
app567 <magicapp1248@gmail.com>
2018-01-31 17:37:05 -0600

    Initial Commit

---
+++ xmlParserSwift4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  xmlParserSwift4
+//
+//  Created by Peter on 31/01/18.
+//  Copyright © 2018 petrusnik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+}
+
